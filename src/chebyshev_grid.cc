39b3b34d64c8a86f5eff6396a3b47e0c95e5a712
NikiDob <nikita.dobrynin01@universitadipavia.it>
2026-03-24 14:36:03 +0100

    StandartGrid Chebyshev constructor

--- src/chebyshev_grid.cc
+++ src/chebyshev_grid.cc
@@ -3,6 +3,16 @@
 namespace Interpolation
 {
 namespace Chebyshev
+StandardGrid::StandardGrid(size_t p)
+{
+    _p = p;
+    for (size_t j = 0; j <= p; j++) {
+        _tj.push_back(cos(j * M_PI / static_cast<double>(p)));
+    }
+    for (size_t j = 0; j <= p; j++) {
+        
+    }
+}
 {
 } // namespace Chebyshev
 } // namespace Interpolation
